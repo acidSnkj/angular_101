3e0ca92e54ef4b1d2d7e673bc1ffafdb702ecb9f
Jorge Rivera <jorgeriv@protonmail.com>
2023-04-25 20:36:34 -0500

    Add a simple class css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Product } from './product.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   person = {
