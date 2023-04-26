b84432518b9ffdc8a7159de9f3749d62c7c89bed
Jorge Rivera <jorgeriv@protonmail.com>
2023-04-25 19:50:46 -0500

    *ngFor for arrays - interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,19 @@ export class AppComponent {
   }
   names: string[] = ['John', 'Doe', 'Jane', 'Meryl'];
   newName: string = '';
+  products: Product[] = [
+    {
+      name: 'Phone XL',
+      price: 799,
+      image: 'https://via.placeholder.com/350x150',
+      category: 'Phone'
+    },
+    {
+      name: 'Phone Mini',
+      price: 699,
+      image: 'https://via.placeholder.com/350x150'
+    }
+  ]
 
   addNewName() {
     this.names.push(this.newName);
